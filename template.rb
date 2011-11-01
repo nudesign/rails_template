63504c42ddff04fac8234090bc59989a2116b8f0
Tiago Rafael Godinho <tiagogodinho3@gmail.com>
2011-11-01 12:20:11 -0200

    alteração no template para configurar o mongoid

--- template.rb
+++ template.rb
@@ -1,9 +1,18 @@
 remove_file 'Gemfile'
 remove_dir 'test'
 
+gsub_file 'config/application.rb', "require 'rails/all'", <<-requires
+require 'action_controller/railtie'
+require 'action_mailer/railtie'
+require 'active_resource/railtie'
+require 'sprockets/railtie'
+requires
+
 get 'https://github.com/nudesign/rails_template/raw/master/Gemfile', 'Gemfile'
 
 run 'bundle'
+
+generate 'mongoid:config'
 
 git :init
 git :add => '.'
