6e3ce9320ba571fb7212aecb825865209ed3cc7f
lucas renan <contato@lucasrenan.com>
2012-12-23 16:07:03 -0200

    spec helper - include factory girl sintax methods

--- spec_helper.rb
+++ spec_helper.rb
@@ -25,6 +25,7 @@ RSpec.configure do |config|
   # rspec-rails.
   config.infer_base_class_for_anonymous_controllers = false
 
+  config.include FactoryGirl::Syntax::Methods
   config.include Devise::TestHelpers, type: :controller
   config.extend ControllerMacros,     type: :controller
 
